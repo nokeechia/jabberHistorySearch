46e713b6c010efd6a79a9cfd28fd6e7e1333c70a
U6019995 <London005>
2015-12-22 14:16:42 +0000

    added, command line params for the database URI

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
+import sys
+import os
 
 app = Flask(__name__)
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///keech.achara@thomsonreuters.com.db'
+print(os.path.join('sqlite:///', sys.argv[1]))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + sys.argv[1]
 db = SQLAlchemy(app)
 
 
